5950f1d782e0c2a54658ba9ad9688340f0aa2790
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2022-08-12 16:46:38 +0700

    [UdemyCourse] Add goal and render in list

--- UdemyCourse/App.js
+++ UdemyCourse/App.js
@@ -1,17 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default App = () => {
+
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (enterText) => {
+    setGoal(enterText);
+  }
+
+  const addGoalHandler = () => {
+    setGoals(currentGoals => [...currentGoals, goal]);
+    setGoal('');
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />  
-        <Button title="Add Goal"/>
+        <TextInput value={goal} onChangeText={goalInputHandler} style={styles.textInput} placeholder="Your course goal!" />  
+        <Button onPress={addGoalHandler} title="Add Goal"/>
       </View>
       <View style={styles.goalsContainer}>
         <Text>
-          List of goals...
+          Goals:
         </Text>
+        <View>
+            {
+              goals?.map((goalItem, key) => {
+                return <View style={styles.goalItem} key={key}><Text style={styles.goalItemText}>{goalItem}</Text></View>
+              })
+            }
+          </View>
       </View>
     </View>
   );
@@ -37,5 +57,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 6,
+  },
+  goalItem: {
+    margin: 5,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#5e0acc',
+  },
+  goalItemText: {
+    color: '#fff',
   },
 });
